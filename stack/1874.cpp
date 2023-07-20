3bb6924b356047c00d26fe255a9f5fc9436b2c4b
178kg78cm <101550897+178kg78cm@users.noreply.github.com>
2023-07-20 15:10:30 +0900

    stack

---
+++ stack/1874.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    stack<char> s;
+    int max=1, min=1;
+    int n, data;
+    cin >> n;
+    int sequence[100000];
+    char list[200000];
+    for(int i=0;i<n;i++){
+        cin >> data;
+        for(int )
+    }
+}
